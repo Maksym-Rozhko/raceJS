155649ae7a46ce0ba3378d3ea4f9c6b56dbf52df
Maksym <maksymRozhko7@gmail.com>
2020-09-10 13:46:32 +0300

    added logic when cars collide

--- main.js
+++ main.js
@@ -31,6 +31,7 @@ function getQuantityElements(heightElement) {
 
 function startGame() {
     start.classList.add('hide');
+    gameArea.innerHTML = '';
     for(let i = 0; i < getQuantityElements(100); i++) {
         const line = document.createElement('div');
         line.classList.add('line');
@@ -49,15 +50,22 @@ function startGame() {
         gameArea.appendChild(enemy);
     }
 
+    setting.scrore = 0;
     setting.start = true;
     gameArea.appendChild(car);
+    car.style.left = gameArea.offsetWidth / 2 - car.offsetWidth / 2;
+    car.style.top = 'auto';
+    car.style.bottom = '10px';
     setting.x = car.offsetLeft;
     setting.y = car.offsetTop;
     requestAnimationFrame(playGame);
 }
 
 function playGame() {
+
     if (setting.start) {
+        setting.scrore += setting.speed;
+        score.innerHTML = 'SCORE<br>' + setting.scrore;
         moveRoad();
         moveEnemy();
         if (keys.ArrowLeft && setting.x > 0) {
@@ -101,7 +109,21 @@ function moveRoad() {
 
 function moveEnemy() {
     let enemy = document.querySelectorAll('.enemy');
+
     enemy.forEach(function(item) {
+        let carRect = car.getBoundingClientRect();
+        let enemyRect = item.getBoundingClientRect();
+
+        if (carRect.top <= enemyRect.bottom && 
+            carRect.right >= enemyRect.left &&
+            carRect.left <= enemyRect.right &&
+            carRect.bottom >= enemyRect.top) {
+            setting.start = false;
+            console.warn('crash');
+            start.classList.remove('hide');
+            start.style.top = score.offsetHeight; 
+        }
+
         item.y += setting.speed / 2;
         item.style.top = item.y + 'px';
         if (item.y > document.documentElement.clientHeight) {
